d2313d06d197cd625626eb96ef8e488f83d8abd1
Neva <nevao@uw.edu>
2024-08-05 14:26:02 -0700

    Edit comments and delete old commented out code

--- plot_num_subjs.py
+++ plot_num_subjs.py
@@ -1,12 +1,11 @@
 ##########
-# function that plots number of subjects per age and gender
+# Plots number of subjects per age and gender
 ##########
 
 import seaborn as sns
 import numpy as np
 from matplotlib import pyplot as plt
 def plot_num_subjs(df, title, struct_var, timept, path):
-    # sns.set(font_scale=2)
     sns.set_style(style='white')
     g = sns.catplot(x="age", hue="sex", data=df, kind="count", legend=False, palette=sns.color_palette(['green', 'blue']))
     g.fig.suptitle(title, fontsize=20)
@@ -15,13 +14,10 @@ def plot_num_subjs(df, title, struct_var, timept, path):
     g.ax.set_ylabel("Number of Subjects", fontsize=18)
     g.ax.tick_params(axis='x', labelsize=16)
     g.ax.tick_params(axis='y', labelsize=16)
-    # g.fig.suptitle(title)
     hue_labels = ['female', 'male']
     g.add_legend(legend_data={
         key: value for key, value in zip(hue_labels, g._legend_data.values())
         }, fontsize=18)
     g.ax.set(yticks=np.arange(0,20,2))
-    # g.set(title=title)
     plt.show(block=False)
     plt.savefig('{}/data/{}/plots/NumSubjects_{}'.format(path, struct_var, timept))
-    #plt.close(g.fig)
