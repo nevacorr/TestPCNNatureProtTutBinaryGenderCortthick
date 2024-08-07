0dc2327141d0cd45de800ab138ef4657d6744fde
Neva <nevao@uw.edu>
2024-08-07 10:43:01 -0700

    Edit comments and remove unused code

--- Calculate_effect_Size_and_CI_using_Zscore.py
+++ Calculate_effect_Size_and_CI_using_Zscore.py
@@ -1,5 +1,5 @@
 ####
-# This program calculate the mean Z score and confidence intervals for males and females. This is one estimate
+# This program calculate the mean Z score and confidence intervals for males and females. This is an estimate
 # of effect size.
 ####
 
@@ -16,10 +16,10 @@ Z_time2_file = f'{working_dir}/predict_files/cortthick/Z_scores_by_region_postco
 # Load Z scores from post-covid data
 Z2 = pd.read_csv(Z_time2_file)
 
-# Add sex to dataframe
+# Extract subject ID numbers
 subject_id = Z2['participant_id'].tolist()
 
-# Calculate sex for each subject
+# Calculate sex for each subject (males are odd numbers, females are even)
 sex_val = [0 if s % 2 == 0 else 1 for s in subject_id]
 
 # Add to dataframe
@@ -52,11 +52,11 @@ Z2_stats.loc['upper_CI_male',:] = (
 Z2_stats.loc['lower_CI_male',:] = (
         Z2_stats.loc['mean_male',:] - 1.96 * Z2_stats.loc['std_male'] / math.sqrt(Z2_male.shape[0] - 1))
 
-# Calculate Cohen's d for effect size
-cohensd_female = Z2_stats.loc['mean_female']#/Z2_stats.loc['std_female']
-cohensd_male = Z2_stats.loc['mean_male']#/Z2_stats.loc['std_male']
+# Calculate effect size
+cohensd_female = Z2_stats.loc['mean_female']
+cohensd_male = Z2_stats.loc['mean_male']
 
-# Count cohen's d values above or equal to 0.5
+# Count effect size values above or equal to 0.5
 
 female_count_above_threshold = (cohensd_female <= -0.5).sum()
 male_count_above_threshold = (cohensd_male <= -0.5).sum()
@@ -72,14 +72,14 @@ lower_ci_female = Z2_stats.loc['lower_CI_female']
 upper_ci_male = Z2_stats.loc['upper_CI_male']
 lower_ci_male = Z2_stats.loc['lower_CI_male']
 
-# Plotting
+# Plot effect size with confidence intervals
 fig, axs = plt.subplots(2, figsize=(14, 18), constrained_layout=True)
 
-# Plotting mean values with error bars for males
+# Plot mean values with CI error bars for males
 axs[1].errorbar(x=range(len(mean_male)), y=mean_male, yerr=[mean_male - lower_ci_male,
                                                 upper_ci_male - mean_male], fmt='o', label='Males', color='blue', markersize=3)
 
-# Plotting mean values with error bars for females
+# Plot mean values with CI error bars for females
 axs[0].errorbar(x=range(len(mean_female)), y=mean_female, yerr=[mean_female - lower_ci_female,
                                                 upper_ci_female - mean_female], fmt='o', label='Females', color='crimson', markersize=3)
 for ax in [0, 1]:
@@ -88,7 +88,6 @@ for ax in [0, 1]:
         gender = 'Males'
     else:
         gender = 'Females'
-    # axs[ax].set_title(f'{gender}: Mean Z-score with Confidence Intervals by Brain Region ')
 
     axs[ax].set_xticks(range(len(mean_female)), mean_female.index, rotation=90, fontsize=11)
     axs[ax].set_xlim(-0.8, len(mean_female) - 0.5)
@@ -100,7 +99,7 @@ for ax in [0, 1]:
 plt.savefig(f'{working_dir}/Mean Effect Size with Confidence Intervals for both genders.pdf', dpi=300, format='pdf')
 plt.show()
 
-# Plot Cohen's d
+# Plot effect size without error bars
 fig, axs =plt.subplots(2, constrained_layout=True, figsize=(14, 18),)
 axs[1].plot(cohensd_male, marker='o', color='b', linestyle='None', label='Males')
 axs[0].plot(cohensd_female, marker='o', color='crimson', linestyle='None',  label='Females')
@@ -110,7 +109,7 @@ for ax in [0, 1]:
         gender = 'Males'
     else:
         gender = 'Females'
-    # axs[ax].set_title(f"{gender}: Effect Size by Brain Region", fontsize=16)
+
     axs[ax].set_xticks(range(len(mean_female)), mean_female.index, rotation=90, fontsize=14)
     axs[ax].set_xlim(-0.8, len(mean_female) - 0.5)
     axs[ax].set_ylim(-1.4, 0.6)
